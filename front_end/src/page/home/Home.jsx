bffd99f1f9aec22187ec8e6f52211d0702aca936
Minh <nguyennhatminh02042006@gmail.com>
2026-06-14 10:05:08 +0700

    Add Home.jsx

---
+++ front_end/src/page/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      {/* Add your home page sections here */}
+      {/* Examples: Banner, Courses, Engagements, Features, Testimonials, etc. */}
+      <h1>Welcome to LearnOva</h1>
+    </div>
+  );
+};
+
+export default Home;
